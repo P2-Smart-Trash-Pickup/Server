15c1cb9a0c3133519060fd7b85d219895a42c601
Alehandre420 <alexandernikolajlarsen@gmail.com>
2025-04-15 12:36:18 +0200

    hejmartin.getchildren

--- mqtt/sub.py
+++ mqtt/sub.py
@@ -2,41 +2,68 @@ import sys,json,time,dotenv,os
 from influxdb_client_3 import InfluxDBClient3, Point
 
 import paho.mqtt.client as paho
+import certifi
 
 dotenv.load_dotenv()
 
 token = os.getenv("TOKEN")
 
 org = "Gruppe 251"
-host = "https://eu-central-1-1.aws.cloud2.influxdata.com/"
+host = "https://eu-central-1-1.aws.cloud2.influxdata.com"
 
-db_client = InfluxDBClient3(host=host, token=token, org=org)
+db_client = InfluxDBClient3(host=host, token=token, org=org,ssl_ca_cert=certifi.where())
 
-database="trivia"
+database="super"
 
 def message_handling(client, userdata, msg):
     global db_client, write,database
-    input_str = msg.payload.decode()
-    print(input_str)
-    input_obj = json.loads(input_str)
+    if msg.topic=="get_data":
+        input_str = msg.payload.decode()
+        print(input_str)
+        input_obj = json.loads(input_str)
 
-    print(input_obj["topic"])
-    print(input_obj["question"])
-    print(input_obj["answer"])
+        name = input_obj["Name"]
+        
+        query = f"SELECT * FROM \"trivia\" WHERE \"Name\"=\'{name}\'"
+        print(query)
+        #Execute the query
+        table = db_client.query(query=query, database=database, language='sql')
 
-    point = (
-            Point("trivia")
-            .tag("topic",input_obj["topic"])
-            .field(input_obj["question"],input_obj["answer"])
-    )
-    db_client.write(database=database,record=point)
-    time.sleep(1)
-    print("Succesfully wrote to database")
+        print("SUI")
+
+        #Convert to dataframe
+        df = table.to_pandas().sort_values(by="time")
+        print(df)
+        
+        time.sleep(1)
+    elif msg.topic=="test_topic":
+        input_str = msg.payload.decode()
+        print(input_str)
+        input_obj = json.loads(input_str)
+
+        print(input_obj["Name"])
+        print(input_obj["Weight"])
+        print(input_obj["Light Distance"])
+        print(input_obj["Sound Distance"])
+
+        point = (
+                Point("trivia")
+                .tag("Name",input_obj["Name"])
+                .field("Weight",input_obj["Weight"])
+                .field("Light Distance",input_obj["Light Distance"])
+                .field("Sound Distance",input_obj["Sound Distance"])
+                
+        )
+        db_client.write(database=database,record=point)
+        time.sleep(1)
+        print("Succesfully wrote to database")
+
 
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("test_topic",qos=0)
+    client.subscribe("get_data",qos=0)
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print(f"Subscribed to topic (QoS: {granted_qos})")
